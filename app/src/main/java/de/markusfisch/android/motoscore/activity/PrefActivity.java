cb4451dce370ce9c8d4c7cad3db8d0a17dde9437
Markus Fisch <mf@markusfisch.de>
2020-08-28 22:43:21 +0200

    Move importing database into its own class

--- app/src/main/java/de/markusfisch/android/motoscore/activity/PrefActivity.java
+++ app/src/main/java/de/markusfisch/android/motoscore/activity/PrefActivity.java
@@ -1,22 +1,16 @@
 package de.markusfisch.android.motoscore.activity;
 
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import de.markusfisch.android.motoscore.R;
-import de.markusfisch.android.motoscore.app.MotoScoreApp;
 import de.markusfisch.android.motoscore.io.DatabaseExporter;
+import de.markusfisch.android.motoscore.io.DatabaseImporter;
 import de.markusfisch.android.motoscore.preference.Preferences;
 import de.markusfisch.android.motoscore.service.MotoScoreService;
 
@@ -41,7 +35,7 @@ public class PrefActivity extends AppCompatPreferenceActivity {
 		if (requestCode == PICK_FILE_RESULT_CODE &&
 				resultCode == RESULT_OK && resultData != null) {
 			Toast.makeText(this,
-					importDatabase(this, resultData.getData()) ?
+					DatabaseImporter.importDatabase(this, resultData.getData()) ?
 							R.string.successfully_imported :
 							R.string.no_rides,
 					Toast.LENGTH_LONG).show();
@@ -112,46 +106,5 @@ public class PrefActivity extends AppCompatPreferenceActivity {
 	@Override
 	protected boolean isValidFragment(String fragmentName) {
 		return false;
-	}
-
-	private static boolean importDatabase(Context context, Uri uri) {
-		if (uri == null) {
-			return false;
-		}
-		ContentResolver cr = context.getContentResolver();
-		if (cr == null) {
-			return false;
-		}
-		final String fileName = "import.db";
-		InputStream in = null;
-		OutputStream out = null;
-		try {
-			in = cr.openInputStream(uri);
-			if (in == null) {
-				return false;
-			}
-			out = context.openFileOutput(fileName, Context.MODE_PRIVATE);
-			byte[] buffer = new byte[4096];
-			int len;
-			while ((len = in.read(buffer)) != -1) {
-				out.write(buffer, 0, len);
-			}
-		} catch (IOException e) {
-			return false;
-		} finally {
-			try {
-				if (in != null) {
-					in.close();
-				}
-				if (out != null) {
-					out.close();
-				}
-			} catch (IOException e) {
-				// ignore, can't do anything about it
-			}
-		}
-		boolean success = MotoScoreApp.db.importDatabase(context, fileName);
-		context.deleteFile(fileName);
-		return success;
 	}
 }
